20098f1856208afdcf0ecbaeb255b1e2cdab4930
ilya <fakeemail@fakeemail.com>
2025-04-12 17:29:42 +0300

    edited protocol.h, DatabaseManager
    made the protocol.h and CharacterData same as in client

--- character_server/include/database_manager.h
+++ character_server/include/database_manager.h
@@ -13,26 +13,6 @@
 
 #include "protocol.h"
 
-struct CharacterData {
-    int32_t id = 0;
-    std::string name{};
-    std::string surname{};
-    uint8_t age = 1;
-    std::string bio{};
-
-    // Serialize/deserialize
-    std::vector<uint8_t> serialize() const;
-    static CharacterData deserialize(const std::vector<uint8_t>& data);
-
-    // Helper methods to serialize/deserialize all characters
-    static std::vector<uint8_t> serializeVector(const std::vector<CharacterData>& characters);
-    static std::vector<CharacterData> deserializeVector(const std::vector<uint8_t>& data);
-
-    // Helper methods
-    static void write_string(std::vector<uint8_t>& buffer, const std::string& str);
-    static std::string read_string(const std::vector<uint8_t>& buffer, size_t& offset);
-};
-
 class DatabaseManager {
 public:
     static DatabaseManager& getInstance();
